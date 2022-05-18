2441ded9c64df51f5f3cbc0fb3606387a2e9315a
Ongezwa <ongezwaf27@gmail.com>
2022-05-18 15:49:44 +0200

    taxi total fare

---
+++ index.js
@@ -0,0 +1,36 @@
+
+        //,
+        // routes(trip){
+        //     let routeCost = this.trips(trip);
+        //     this.taxiFare += routeCost;
+        // }
+    //  {
+    //         name:'Paarl',
+    //         totalFarePaarl:12.95,
+    //         passengersPaarl:0.00,
+    //         taxiTripsPaarl:0.00,
+    //         availTaxiPaarl:3,
+    //     },
+
+    //     {
+    //         name:'Athlone',
+    //         totalFareAth:15.95,
+    //         passengersAth:0.00,
+    //         taxiTripsAth:0.00,
+    //         availTaxiAth:3,
+    //     },
+
+    //     {
+    //         name:'Parow',
+    //         totalFareParow:14.50,
+    //         passengersParow:0.00,
+    //         taxiTripsParow:0.00,
+    //         availTaxiParow:3,
+    //     },
+
+    //     {
+    //         name:'Danoon',
+    //         totalFareDan:0.00,
+    //         passengersDan:0.00,
+    //         taxiTripsDan:0.00,
+    //         availTaxiDan:3, 
